b0116552ede035de147615e957ace7be7ee98348
astradzhao <astradzhao@gmail.com>
2024-08-28 01:27:13 -0700

    new image

--- personal-website/src/pages/SightsPage.js
+++ personal-website/src/pages/SightsPage.js
@@ -34,14 +34,6 @@ function SightsPage() {
                 gridArea="3 / 4 / 4 / 5"
             />
             <ImageTemplate
-                src={`${process.env.PUBLIC_URL}/files/images/sokcho_dock.png`}
-                caption="Dock View"
-                date="October 1, 2023"
-                location="Sokcho (속초), South Korea"
-                height="20vmax"
-                gridArea="4 / 1 / 5 / 2"
-            />
-            <ImageTemplate
                 src={`${process.env.PUBLIC_URL}/files/images/tokyo_red_alley.png`}
                 caption="Red Alley"
                 date="December 20, 2023"
@@ -81,14 +73,6 @@ function SightsPage() {
                 gridArea="3 / 2 / 4 / 3"
             />
             <ImageTemplate
-                src={`${process.env.PUBLIC_URL}/files/images/tokyo_street.png`}
-                caption="Tokyo City Life"
-                date="December 20, 2023"
-                location="Tokyo, Japan"
-                height="20vmax"
-                gridArea="4 / 4 / 5 / 5"
-            />
-            <ImageTemplate
                 src={`${process.env.PUBLIC_URL}/files/images/muir_bridge.png`}
                 caption="Forest Bridge"
                 date="July 28, 2024"
@@ -96,7 +80,7 @@ function SightsPage() {
                 height="20vmax"
                 zoom="1.6"
                 y="-65%"
-                gridArea="4 / 2 / 5 / 4"
+                gridArea="4 / 3 / 5 / 5"
             />
             <ImageTemplate
                 src={`${process.env.PUBLIC_URL}/files/images/muir_trees.png`}
@@ -105,6 +89,17 @@ function SightsPage() {
                 location="Muir Woods National Monument, California"
                 height="20vmax"
                 gridArea="2 / 2 / 3 / 3"
+            />
+            <ImageTemplate
+                src={`${process.env.PUBLIC_URL}/files/images/sf_airbnb.png`}
+                caption="Apartment View"
+                date="August 26, 2024"
+                location="San Francisco, California"
+                height="20vmax"
+                zoom="1.6"
+                y="-65%"
+                x="-45%"
+                gridArea="4 / 1 / 5 / 3"
             />
         </div>
       </div>
