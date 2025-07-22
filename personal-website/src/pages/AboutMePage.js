4758587c2efd5e3a2b9b203b2c0d9a4136691f1a
astradzhao <astradzhao@gmail.com>
2025-07-21 20:38:29 -0700

    changes website

--- personal-website/src/pages/AboutMePage.js
+++ personal-website/src/pages/AboutMePage.js
@@ -6,9 +6,9 @@ function AboutMePage() {
   return (
     <div className='page-container'>
       <p className='page-text2'>I'm currently a MS CS student at UCSD, with a research focus in ML Sys, alternative NLP, and increasing global access to AI.</p>
-      <p className='page-text2'>I am also currently a TA for the Spring 2025 offering of <a href="https://cseweb.ucsd.edu/classes/sp25/cse253-a/" target="_blank" rel="noopener noreferrer" className="resume-link">CSE 153/253: Machine Learning for Music</a> under Professor <a href="https://cseweb.ucsd.edu/~jmcauley/" target="_blank" rel="noopener noreferrer" className="resume-link">Julian McAuley</a>.</p>
-      <p className='page-text2'>In the upcoming summer (2025), I will be interning at Qualcomm, where I will be working on the development of a multimodal QA system.</p>
+      <p className='page-text2'>I am also interning at Qualcomm, where I am working on the development of a low-resource multimodal RAG QA system through Langchain, to be deployed on a Qualcomm-native Adreno GPU device.</p>
       <br></br>
+      <p className='page-text3'>In spring 2025, I was a TA for <a href="https://cseweb.ucsd.edu/classes/sp25/cse253-a/" target="_blank" rel="noopener noreferrer" className="resume-link">CSE 153/253: Machine Learning for Music</a> under Professor <a href="https://cseweb.ucsd.edu/~jmcauley/" target="_blank" rel="noopener noreferrer" className="resume-link">Julian McAuley</a>.</p>
       <p className='page-text3'>In winter 2025, I was a TA for <a href="https://hao-ai-lab.github.io/cse234-w25/" target="_blank" rel="noopener noreferrer" className="resume-link">CSE 234: Data Systems for ML</a> under Professor <a href="https://cseweb.ucsd.edu/~haozhang/" target="_blank" rel="noopener noreferrer" className="resume-link">Hao Zhang</a> at UCSD.</p>
       <p className='page-text3'>In spring 2024, I graduated from UC Berkeley with two B.As, one in Computer Science and one in Applied Math. Gob ears 🧸!</p>
       <p className='page-text3'>Before my interest in research, I worked as a software engineer intern at Accretional, Hinge Health, and Cisco Meraki. I was a longtime tutor for a Bay Area-based CS tutoring startup called Breakout Mentors.</p>
